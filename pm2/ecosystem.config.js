84e9ce6b319c7058071c60f4c35628d5888e9272
Ethan Chu <agaiiixs@gmail.com>
2026-03-26 00:49:39 +0800

    Change cwd to use HOME environment variable

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Shiroi',
       script: './server.js',
-      cwd: __dirname,
+      cwd: process.env.HOME + '/shiro/current',
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
